c1b98031e06332943bd667b229f61cfcb46b07a3
JSLee <youso215@naver.com>
2014-07-29 01:29:13 +0900

    new

---
+++ withit/src/spring/board/vo/Board.java
@@ -0,0 +1,34 @@
+package spring.board.vo;
+
+public class Board {
+	
+	private String userid;
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUserpwd() {
+		return userpwd;
+	}
+	public void setUserpwd(String userpwd) {
+		this.userpwd = userpwd;
+	}
+	private String userpwd;
+	
+//	public String getTitle() {
+//		return title;
+//	}
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//	public String getContent() {
+//		return content;
+//	}
+//	public void setContent(String content) {
+//		this.content = content;
+//	}
+
+}
